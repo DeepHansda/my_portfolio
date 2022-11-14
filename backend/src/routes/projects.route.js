9aae204f2b2a7540079cee6375b41beebfe2a0cc
Deep Hansda <deephansda921@gmail.com>
2022-11-14 10:32:55 +0530

    working on project images.

--- backend/src/routes/projects.route.js
+++ backend/src/routes/projects.route.js
@@ -3,7 +3,7 @@ const router = new express.Router();
 const {uploadProjects,showProjects,deleteProject} = require('../controllers/projects.controller')
 const {upload} = require('../services/projects.service')
 
-router.post('/uploadProject',upload.array("img",3),uploadProjects)
+router.post('/uploadProject',upload.array("img",4),uploadProjects)
 router.get('/getProjects',showProjects)
 router.post('/deleteProject/:id',deleteProject)
 
