512873738643bdfc9f3ebc5ce46f8738a6a153d5
Deep Hansda <deephansda921@gmail.com>
2025-01-15 18:43:05 +0530

    feat(frontend):switch from react.js to next.js

--- client/src/Redux/Common/http_common.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios'
-// http://localhost:3400/api/
-// https://my-portfolio-backend-deephansda.vercel.app/api
-export default axios.create({
-    baseURL:'https://my-portfolio-backend-deephansda.vercel.app/api',
-    headers: {
-        "Content-Type": 'application/json'
-    }
-})
