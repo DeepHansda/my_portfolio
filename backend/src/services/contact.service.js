b377a5e4ee613bd57832e4a152cadcbca4f4dde1
DeepHansda <deephansda921@gmail.com>
2025-04-19 12:53:53 +0530

    Chore(backend):update mail system for better mailing

--- backend/src/services/contact.service.js
+++ backend/src/services/contact.service.js
@@ -1,58 +1,42 @@
 const nodemailer = require("nodemailer");
 const handlebars = require("handlebars");
-var fs = require("fs");
+const fs = require("fs").promises; // use promise version of fs
 const path = require("path");
 const dotenv = require("dotenv");
 dotenv.config();
 
-var readHTMLFile = function (path, callback) {
-  fs.readFile(path, { encoding: "utf-8" }, function (err, html) {
-    if (err) {
-      callback(err);
-      throw err;
-    } else {
-      callback(null, html);
-    }
-  });
+const mailService = async (data) => {
+  try {
+    const transporter = nodemailer.createTransport({
+      host: "smtp.gmail.com",
+      port: 465,
+      secure: true,
+      auth: {
+        user: process.env.MAIL_USERNAME,
+        pass: process.env.MAIL_PASSWORD,
+      },
+    });
+
+    const htmlPath = path.join(__dirname, "../templates/mail.html");
+    const html = await fs.readFile(htmlPath, "utf-8");
+
+    const template = handlebars.compile(html);
+    const htmlToSend = template({ fullName: data.fullName });
+
+    const mailOptions = {
+      from: `Deep Hansda <${process.env.MAIL_USERNAME}>`,
+      to: data.email,
+      subject: "Message From Deep Hansda",
+      html: htmlToSend,
+    };
+
+    const result = await transporter.sendMail(mailOptions);
+    console.log("Mail sent:", result);
+    return result;
+  } catch (err) {
+    console.error("Mail error:", err);
+    throw err; // allow error to be caught by calling function
+  }
 };
 
-const mailService = (data, callback) => {
-  var transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 465,
-    secure: true,
-    auth: {
-      user:process.env.MAIL_USERNAME,
-      pass:process.env.MAIL_PASSWORD,
-    },
-  });
-
-  readHTMLFile(
-    path.join(__dirname, "../templates/mail.html"),
-    (error, html) => {
-      if (error) {
-        console.error(error);
-      }
-      const template = handlebars.compile(html);
-      const replacement = {
-        fullName: data.fullName,
-      };
-      const htmlToSend = template(replacement);
-      let mailOptions = {
-        from: `Deep Hansda <${process.env.MAIL_USERNAME}>`,
-        to: data.email,
-        subject: "Message From Deep hansda",
-        html: htmlToSend,
-      };
-
-      transporter.sendMail(mailOptions, (error, data) => {
-        if (error) {
-          return callback(error);
-        }
-        return callback(null, data);
-      });
-    }
-  );
-};
-
-module.exports = { mailService: mailService };
+module.exports = { mailService };
